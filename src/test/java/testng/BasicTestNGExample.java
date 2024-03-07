c734ce08eae376ef0e171946b9c97722c8544721
vinayak2318 <162452193+vinayak2318@users.noreply.github.com>
2024-03-07 18:23:07 +0530

    Update BasicTestNGExample.java

--- src/test/java/testng/BasicTestNGExample.java
+++ src/test/java/testng/BasicTestNGExample.java
@@ -25,6 +25,7 @@ public class BasicTestNGExample {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.amazon.in/");
+        driver.manage().window().maximize();
         driver.close();
     }
 }
